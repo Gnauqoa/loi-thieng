3182b28cbe1db3c1a9cf9f17e0cd1bd175f405bc
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-11-23 15:17:06 +0700

    Init webRTC and firebase.

---
+++ config/firebase.ts
@@ -0,0 +1,26 @@
+import { initializeFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC-eJXp2x1OovDP6eCgQmVRgUU_gdn3PyY",
+  authDomain: "loi-thieng-fd643.firebaseapp.com",
+  projectId: "loi-thieng-fd643",
+  storageBucket: "loi-thieng-fd643.firebasestorage.app",
+  messagingSenderId: "928570948109",
+  appId: "1:928570948109:web:5e6293632d05e1adfff344",
+  measurementId: "G-N20TWDLFHQ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+});
