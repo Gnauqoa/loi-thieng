b4f042c37925ee3bde372ff0148ad59ae1857e43
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-12-29 19:17:17 +0700

    Add toast and init routers.

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,23 @@
 // In App.js in a new project
 
 import * as React from "react";
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { FirebaseAuthProvider } from "@/src/context/FirebaseAuthContext";
+import { FirebaseAuthProvider } from "@/context/FirebaseAuthContext";
 import TamaguiProvider from "./config/tamagui.config";
-import HomeScreen from "./pages/home";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "./config/redux/store";
+import RouterContainer from "./routers";
 
-const Stack = createNativeStackNavigator();
+import Toast from "react-native-toast-message";
 
 function App() {
   return (
     <FirebaseAuthProvider>
       <ReduxProvider store={store}>
         <TamaguiProvider>
-          <NavigationContainer>
-            <Stack.Navigator>
-              <Stack.Screen name="Home" component={HomeScreen} />
-            </Stack.Navigator>
-          </NavigationContainer>
+          <RouterContainer />
         </TamaguiProvider>
       </ReduxProvider>
+      <Toast />
     </FirebaseAuthProvider>
   );
 }
---
+++ src/routers/path.ts
@@ -0,0 +1,7 @@
+export const PATH_AUTH = {
+  login: "Login",
+};
+
+export const PATH_PAGE = {
+  home: "Home",
+};
