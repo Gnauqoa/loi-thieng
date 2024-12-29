066739e350fbd53595d22f000c0585e47bad3857
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-12-29 22:46:48 +0700

    Add auth screen group.

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -5,13 +5,28 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "@/pages/home";
+import { useAuth } from "@/config/redux/slices/auth";
+import LoginScreen from "@/pages/auth/login";
 
 const Stack = createNativeStackNavigator();
 const RouterContainer = () => {
+  const { isAuthenticated } = useAuth();
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        {isAuthenticated ? (
+          <Stack.Group>
+            <Stack.Screen name="Home" component={HomeScreen} />
+          </Stack.Group>
+        ) : (
+          <Stack.Group
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Group>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
