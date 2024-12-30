f01a8b2d939ca062e0af37a81784d610e3709db5
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-12-30 22:58:11 +0700

    Add tab bar.

---
+++ src/components/TabBar/index.tsx
@@ -0,0 +1,68 @@
+import { View, StyleSheet, Pressable } from "react-native";
+import { PlatformPressable } from "@react-navigation/elements";
+import { Text } from "@tamagui/core";
+import type { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import HomeIcon from "@/assets/icons/home.svg";
+
+function TabBar({
+  state,
+  descriptors,
+  navigation,
+}: BottomTabBarProps): JSX.Element {
+  return (
+    <View className="flex flex-row items-center w-[100vw] py-3 overflow-hidden">
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
+
+        return (
+          <View key={route.key} className="flex-1">
+            <View className="relative">
+              <PlatformPressable
+                accessibilityState={isFocused ? { selected: true } : {}}
+                accessibilityLabel={options.tabBarAccessibilityLabel}
+                testID={options.tabBarTestID}
+                onPress={onPress}
+                onLongPress={onLongPress}
+                className="rounded-full items-center justify-center"
+              >
+                <HomeIcon fill={isFocused ? "#673ab7" : "#222"} />
+                <Text style={{ color: isFocused ? "#673ab7" : "#222" }}>
+                  {label as string}
+                </Text>
+              </PlatformPressable>
+            </View>
+          </View>
+        );
+      })}
+    </View>
+  );
+}
+
+export default TabBar;
