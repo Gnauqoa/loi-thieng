20b8ade6269bd5f3046f66baaaeb8c3540778b7a
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-12-30 15:27:07 +0700

    Add login page.

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,9 +1,10 @@
 import { login, useAuth } from "@/config/redux/slices/auth";
 import { SafeAreaView, View } from "react-native";
-import { Input, Text, Button, Form, InputProps, Spinner } from "tamagui";
-import { useState } from "react";
+import { Button, Spinner, Image, useWindowDimensions } from "tamagui";
+import { useEffect, useState } from "react";
 import { toastError } from "@/utils/toast";
 import TextField, { createHandleChangeForm } from "@/components/TextFIeld";
+import bg from "@/assets/login/background.png";
 
 const LoginScreen = () => {
   const { isLoading, dispatch } = useAuth();
@@ -12,6 +13,9 @@ const LoginScreen = () => {
     account: "",
     password: "",
   });
+  const { width } = useWindowDimensions();
+  const widthImg = width / 1.1;
+  const heightImg = (widthImg * 1152) / 896;
 
   const handleChange = createHandleChangeForm(setFormData);
 
@@ -30,28 +34,40 @@ const LoginScreen = () => {
   };
 
   return (
-    <SafeAreaView className="flex flex-col w-full h-full px-4 justify-center gap-3">
-      <TextField
-        label="Tài khoản (email hoặc tên đăng nhập)"
-        value={formData.account}
-        nativeID="account"
-        onChangeText={(value) => handleChange("account", value)}
-      />
-      <TextField
-        label="Mật khẩu"
-        value={formData.password}
-        onChangeText={(value) => handleChange("password", value)}
-        textContentType="password"
-        secureTextEntry={true}
+    <SafeAreaView className="flex flex-col p-5 overflow-hidden w-full h-full justify-center items-center relative ">
+      <Image
+        src={bg}
+        style={{
+          width: widthImg,
+          height: heightImg,
+          objectFit: "cover",
+        }}
       />
 
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <Button disabled={isLoading} onPress={handleLogin}>
-          Đăng nhập
-        </Button>
-      )}
+      <View className="absolute z-20 flex flex-col gap-4 h-full w-full justify-center px-24">
+        <TextField
+          label="Tài khoản (email hoặc tên đăng nhập)"
+          value={formData.account}
+          width={`${widthImg - 100}px`}
+          onChangeText={(value) => handleChange("account", value)}
+        />
+        <TextField
+          label="Mật khẩu"
+          value={formData.password}
+          width={`${widthImg - 100}px`}
+          onChangeText={(value) => handleChange("password", value)}
+          textContentType="password"
+          secureTextEntry={true}
+        />
+
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <Button disabled={isLoading} onPress={handleLogin}>
+            Đăng nhập
+          </Button>
+        )}
+      </View>
     </SafeAreaView>
   );
 };
