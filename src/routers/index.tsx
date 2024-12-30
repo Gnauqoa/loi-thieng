09c4f595156c3a90376c2767d39def2cd7cb858e
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-12-30 15:49:02 +0700

    Add auto login.

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -5,29 +5,35 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "@/pages/home";
-import { useAuth } from "@/config/redux/slices/auth";
+import { initializeAuth, useAuth } from "@/config/redux/slices/auth";
 import LoginScreen from "@/pages/auth/login";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import TabBar from "@/components/TabBar";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 const RouterContainer = () => {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, dispatch } = useAuth();
+
+  React.useEffect(() => {
+    dispatch(initializeAuth());
+  }, []);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        {isAuthenticated ? (
-          <Stack.Group>
-            <Stack.Screen name="Home" component={HomeScreen} />
-          </Stack.Group>
-        ) : (
-          <Stack.Group
-            screenOptions={{
-              headerShown: false,
-            }}
-          >
-            <Stack.Screen name="Login" component={LoginScreen} />
-          </Stack.Group>
-        )}
-      </Stack.Navigator>
+      {isAuthenticated ? (
+        <Tab.Navigator
+          screenOptions={{ headerShown: false }}
+          tabBar={(props) => <TabBar {...props} />}
+        >
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Home2" component={HomeScreen} />
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 };
