4b3e2934a3ca5ffaa36d4da24ad356f5b9ac8bc3
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-12-30 22:57:22 +0700

    Init react native svg.

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,19 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname);
+let config = getDefaultConfig(__dirname);
 
+// Adjust the resolver and transformer to include react-native-svg-transformer
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
+};
+
+config.resolver = {
+  ...config.resolver,
+  assetExts: config.resolver.assetExts.filter(ext => ext !== "svg"),
+  sourceExts: [...config.resolver.sourceExts, "svg"],
+};
+
+// Add NativeWind configuration
 module.exports = withNativeWind(config, { input: "./global.css" });
