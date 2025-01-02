58be9f16653fc2ecb4de867b12695a7bcb6fa971
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2025-01-02 19:23:24 +0700

    Update format.

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,11 +1,12 @@
-import { AxiosResponse } from 'axios';
-import axios from '../utils/axios';
+import { AxiosResponse } from "axios";
+import axios from "../utils/axios";
 
 export const signIn = (payload: object): Promise<AxiosResponse> =>
-  axios.post('/users/users/sign_in', payload);
+  axios.post("/users/users/sign_in", payload);
 
-export const getCurrentUser = (): Promise<AxiosResponse> => axios.get('/users/users/current');
+export const getCurrentUser = (): Promise<AxiosResponse> =>
+  axios.get("/users/users/current");
 export const updateProfile = (payload: object): Promise<AxiosResponse> =>
-  axios.put('/users/users/current', payload);
+  axios.put("/users/users/current", payload);
 export const changePassword = (payload: object): Promise<AxiosResponse> =>
-  axios.put('/users/users/current/password', payload);
+  axios.put("/users/users/current/password", payload);
